70d2b2ed7a6b8186d221f23b068427cb04b60d08
PrinceCodes <princecodes247@gmail.com>
2021-12-21 08:44:59 +0100

    feat: create meme model

---
+++ models/Meme.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const MemeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+   comments: {
+    type: Array,
+    default: [],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+MemeSchema.index({ name: "text" });
+const Meme = mongoose.model("Meme", MemeSchema);
+
+module.exports = Meme;
+;``
