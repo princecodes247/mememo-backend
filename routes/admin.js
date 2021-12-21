f407222e9b7808af56642b3f20b3a527658fa6bc
PrinceCodes <princecodes247@gmail.com>
2021-12-21 22:12:08 +0100

    refactor: clean up admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -7,13 +7,13 @@ const User = require("../models/User");
 
 
 // Welcome Page
-router.get("/", forwardAuthenticated, (req, res) => res.render("welcome"));
+router.get("/", forwardAuthenticated, (req, res) => res.json("Admin"));
 
 router.get("/stats", (req, res) => {
   User.countDocuments()
     .then(userCount => {
 
-      res.render("stats", {
+      res.json( {
         userCount
       })
     })
@@ -25,57 +25,11 @@ router.get("/user/:username", (req, res) => {
 
   User.findOne({ username: req.params.username }).then((person) => {
     if (person) {
-      res.render("quiz", {
-        user: req.user,
-        person: person.name,
+      res.render( {
+        user: person,
       });
     } else {
-      res.render("404");
-    }
-  })
-    .catch(err => console.log(err))
-});
-router.post("/user/:username", (req, res) => {
-
-  User.findOne({ username: req.params.username }).then((person) => {
-    if (person) {
-      message = req.body
-      person.messages.push(message)
-      person.save().then(
-        res.json({
-          message: "Answers sent successfully now it's your turn"
-        })
-      )
-        .catch(err => console.log(err))
-    } else {
-
-      res.json({
-        message: "not found"
-      })
-    }
-  })
-});
-
-router.get("/login", forwardAuthenticated, (req, res) => res.redirect("/users/login"));
-router.get("/register", forwardAuthenticated, (req, res) => res.redirect("/users/register"));
-router.get("/signup", forwardAuthenticated, (req, res) => res.redirect("/users/register"));
-
-
-
-// Dashboard
-router.get("/dashboard", ensureAuthenticated, (req, res) =>
-  res.render("dashboard", {
-    user: req.user,
-  })
-);
-
-router.get("/messages", ensureAuthenticated, (req, res) => {
-
-  User.findOne({ username: req.user.username }).then((person) => {
-    if (person) {
-      res.json({
-        messages: person.messages
-      })
+      res.json("no such user");
     }
   })
     .catch(err => console.log(err))
