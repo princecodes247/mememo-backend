c46d2f0eba34f86387bf37f3c2de989e6c004ec9
PrinceCodes <princecodes247@gmail.com>
2021-12-21 10:23:56 +0100

    feat: update meme routes

---
+++ public/js/toBase64.js
@@ -0,0 +1,8 @@
+function encodeImageFileAsURL(element) {
+  var file = element.files[0];
+  var reader = new FileReader();
+  reader.onloadend = function() {
+    console.log('RESULT', reader.result)
+  }
+  reader.readAsDataURL(file);
+}
